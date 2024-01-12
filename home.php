1f2782a06aa735d45ffcd103b23a10844a48837e
kamluuu3006 <96969057+kamluuu3006@users.noreply.github.com>
2024-01-12 19:24:57 +0530

    edit user, your quotes added

--- home.php
+++ home.php
@@ -199,6 +199,103 @@
 <section class="py-8 mt-6 lg:mt-2 lg:py-16 antialiased">
     <div class="max-w-2xl mx-auto px-4">
         <?php 
+        $animalNames = array(
+            'Luna Lynx',
+            'Stellar Stallion',
+            'Azure Albatross',
+            'Celestial Cheetah',
+            'Safari Serpent',
+            'Nova Nectar',
+            'Polar Panther',
+            'Aurora Avian',
+            'Jungle Jaguar',
+            'Galactic Gecko',
+            'Amber Arctic',
+            'Quasar Quokka',
+            'Savannah Sparrow',
+            'Marine Meerkat',
+            'Whispering Wolf',
+            'Zephyr Zebra',
+            'Ethereal Elephant',
+            'Solar Squirrel',
+            'Luminous Lemur',
+            'Sapphire Seahorse',
+            'Tranquil Toucan',
+            'Mystic Moose',
+            'Cerulean Chameleon',
+            'Frosty Fox',
+            'Lush Llama',
+            'Cosmic Cobra',
+            'Jubilant Jellyfish',
+            'Arctic Armadillo',
+            'Amethyst Aardvark',
+            'Emerald Eagle',
+            'Quirky Quail',
+            'Radiant Raccoon',
+            'Abyssal Alpaca',
+            'Tropical Turtle',
+            'Sphinx Snail',
+            'Pondering Penguin',
+            'Lively Lynx',
+            'Vibrant Vulture',
+            'Serenity Seahorse',
+            'Whimsical Wombat',
+            'Crimson Coyote',
+            'Infinite Iguana',
+            'Zestful Zebra',
+            'Lively Lemur',
+            'Panoramic Parrot',
+            'Bountiful Bobcat',
+            'Velvet Viper',
+            'Majestic Mantis',
+            'Magnetic Marmoset',
+            'Iridescent Ibis',
+            'Nebula Numbat',
+            'Quasar Quail',
+            'Epic Elephant',
+            'Soothing Sloth',
+            'Blissful Butterfly',
+            'Harmonic Hedgehog',
+            'Eclipsed Echidna',
+            'Radiant Rabbit',
+            'Nautical Narwhal',
+            'Jovial Jellyfish',
+            'Zenith Zebra',
+            'Mellow Manatee',
+            'Opulent Owl',
+            'Placid Pangolin',
+            'Furry Ferret',
+            'Lunar Lemur',
+            'Vivid Viper',
+            'Sylvan Salamander',
+            'Whispering Walrus',
+            'Enigmatic Eel',
+            'Quizzical Quokka',
+            'Cascade Chameleon',
+            'Serene Seahorse',
+            'Tranquil Tiger',
+            'Lively Lizard',
+            'Lunar Llama',
+            'Frosty Falcon',
+            'Radiant Raptor',
+            'Gentle Giraffe',
+            'Eclipsed Eagle',
+            'Arctic Albatross',
+            'Mystic Meerkat',
+            'Whimsical Whale',
+            'Placid Pangolin',
+            'Dazzling Dingo',
+            'Celestial Cheetah',
+            'Pondering Puma',
+            'Lagoon Lizard',
+            'Lustrous Lynx',
+            'Soothing Stingray',
+            'Majestic Moth',
+            'Harmonic Hummingbird',
+            'Tranquil Tarsier',
+            'Crimson Crane',
+            'Glowing Gazelle'
+        );
         $getQuotesQuery = "SELECT post_id, quote, date, user_id FROM posts ORDER BY datetime DESC";
         $result = mysqli_query($conn, $getQuotesQuery);
         if (mysqli_num_rows($result) > 0) {
@@ -212,15 +309,23 @@
                 $postusername = $postedquote["name"];
                 $likeQuery = "SELECT like_id FROM likes WHERE post_id = $post_id AND user_id = $user_id";
                 $runQuery = mysqli_query($conn, $likeQuery);
-                
+                if($username != $postusername){
         echo '<article class="p-6 mt-10 lg:mt-5 border shadow-lg text-base bg-white rounded-lg">
             <footer class="flex justify-between items-center mb-2">
                 <div class="flex items-center">
-                    <p class="inline-flex items-center mr-3 text-sm text-gray-900 font-semibold"> '.$postusername.'</p>
+                    <p class="inline-flex items-center mr-3 text-sm text-gray-900 font-semibold"> '.$animalNames[array_rand($animalNames)].'</p>
                     <p class="text-sm text-gray-600"><time pubdate datetime="2022-02-08"
                             title="February 8th, 2022">'.$date.'</time></p>
                 </div>';
-                
+            } else {
+                echo '<article class="p-6 mt-10 lg:mt-5 border shadow-lg text-base bg-white rounded-lg">
+            <footer class="flex justify-between items-center mb-2">
+                <div class="flex items-center">
+                    <p class="inline-flex items-center mr-3 text-sm text-gray-900 font-semibold"> ▲ '.$postusername.'</p>
+                    <p class="text-sm text-gray-600"><time pubdate datetime="2022-02-08"
+                            title="February 8th, 2022">'.$date.'</time></p>
+                </div>';
+            }
         echo '
                 <a href="like.php" type="button"
                     class="flex items-center bg-white text-sm text-gray-500 hover:underline font-medium">
